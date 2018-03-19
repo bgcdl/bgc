9c27bc1b678a21c765033d54d6d30602c5523887
amolynn <amolynn@qq.com>
2018-03-19 14:46:14 +0800

    Project initialization

---
+++ bgc-vm/vm-test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Neo;
+using Neo.VM;
+using Neo.Cryptography;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new ExecutionEngine(null, Crypto.Default);
+            engine.LoadScript(File.ReadAllBytes(@"../../net-core-test/net-core-test/bin/Debug/netstandard2.0/net-core-test.avm")); 
+
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitPush(2); // 对应形参 c
+                sb.EmitPush(4); // 对应形参 b
+                sb.EmitPush(3); // 对应形参 a
+                engine.LoadScript(sb.ToArray());
+            }
+
+            engine.Execute(); // 开始执行
+
+            var result = engine.EvaluationStack.Peek().GetBigInteger(); // 在这里设置返回值
+            Console.WriteLine($"执行结果 {result}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ net-core-test/net-core-test/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+public class Sum : SmartContract
+{
+    public static int Main(int a, int b, int c)
+    {
+        return a + b;
+    }
+}
